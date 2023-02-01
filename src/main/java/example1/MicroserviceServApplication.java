7dc0dc1d7e7c4cf5ca132a392cc826ecd3e6698f
carlede12 <viceroyjua@gmail.com>
2023-02-01 16:47:38 -0600

    adding project

---
+++ src/main/java/example1/MicroserviceServApplication.java
@@ -0,0 +1,13 @@
+package example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example1/MicroserviceServApplicationTests.java
@@ -0,0 +1,13 @@
+package example1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
